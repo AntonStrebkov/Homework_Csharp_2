c74db0c6bd27c18a56f874be591cc7c8c23bf6c4
AntonStrebkov <wancler987@gmail.com>
2022-08-10 20:21:28 +0700

    add Homework in Git

---
+++ Ex001_Middle/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit1 = num % 100;
+int digit2 = digit1 / 10;
+Console.WriteLine(digit2);
+
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine(number);
+// int digit1 = number / 10;
+// int digit2 = digit1 % 10;
+// Console.WriteLine(digit2);
---
+++ Ex002_ThreeDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+int num = new Random().Next(1, 10);
+Console.WriteLine(num);
+string str_num = num.ToString();
+if(str_num.Length > 2)
+    Console.WriteLine("Третья цифра ->" + str_num[2]);
+else
+    Console.WriteLine("Третьей цифры нет");
+
